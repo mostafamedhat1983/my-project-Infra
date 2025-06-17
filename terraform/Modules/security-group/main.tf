01c18b60924fb69140ab3708c0259f2b8d857bc4
mostafamedhat1983 <82119942+mostafamedhat1983@users.noreply.github.com>
2025-06-17 04:47:29 +0300

    adding rules to security groups

--- terraform/Modules/security-group/main.tf
+++ terraform/Modules/security-group/main.tf
@@ -7,3 +7,19 @@ resource "aws_security_group" "this" {
     Name = var.name
   }
 }
+
+resource "aws_security_group_rule" "rules" {
+  for_each = var.rules
+
+  type              = each.value.type
+  security_group_id = aws_security_group.this.id
+  from_port         = each.value.from_port
+  to_port           = each.value.to_port
+  protocol          = each.value.protocol
+  
+  # Use either CIDR blocks OR security group ID
+  cidr_blocks              = each.value.cidr_blocks
+  source_security_group_id = each.value.source_security_group_id
+  
+  description = each.value.description
+}
--- terraform/Modules/security-group/variables.tf
+++ terraform/Modules/security-group/variables.tf
@@ -9,3 +9,15 @@ variable "description" {
 variable "vpc_id" {
   type = string
 }
+
+variable "rules" {
+  type = map(object({
+    type                     = string
+    from_port                = number
+    to_port                  = number
+    protocol                 = string
+    cidr_blocks              = optional(list(string))
+    source_security_group_id = optional(string)
+    description              = string
+  }))
+}
