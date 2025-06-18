8308643a08736ebe349b05c65e6cbba0afe4cf9d
mostafamedhat1983 <82119942+mostafamedhat1983@users.noreply.github.com>
2025-06-18 20:33:10 +0300

    adding s3 bucket name for dev and prod

--- terraform/Dev/backend.tf
+++ terraform/Dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "mybucket"
+    bucket = "dev-backend-5867"
     key    = "dev/tf-state/terraform.tfstate"
     region       = "us-east-2"
     use_lockfile = true
--- terraform/Prod/backend.tf
+++ terraform/Prod/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "mybucket"
+    bucket = "prod-backend-5867"
     key    = "prod/tf-state/terraform.tfstate"
     region       = "us-east-2"
     use_lockfile = true
