313d09780097d254ddc441a9ce49434869b38785
mostafamedhat1983 <82119942+mostafamedhat1983@users.noreply.github.com>
2025-06-21 16:50:05 +0300

    missing " in name varaible in dev env

--- terraform/Dev/variables.tf
+++ terraform/Dev/variables.tf
@@ -8,12 +8,12 @@ variable "subnet_config" {
     "us-east-2a" = {
       cidr_block        = "10.0.1.0/24"
       availability_zone = "us-east-2a"
-      name = nat
+      name = "nat"
     }
     "us-east-2b" = {
       cidr_block        = "10.0.2.0/24"
       availability_zone = "us-east-2b"
-      name = nat
+      name = "nat"
     }
   }
 }
