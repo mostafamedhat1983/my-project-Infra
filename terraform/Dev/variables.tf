0606d27cee79058b207a10f21bc1bb1cb909b70d
mostafamedhat1983 <82119942+mostafamedhat1983@users.noreply.github.com>
2025-06-21 16:40:59 +0300

    add missing name varaible for public subnets in dev env

--- terraform/Dev/variables.tf
+++ terraform/Dev/variables.tf
@@ -2,15 +2,18 @@ variable "subnet_config" {
   type = map(object({
     cidr_block        = string
     availability_zone = string
+    name = string
   }))
   default = {
-    "public_subnet_1" = {
+    "us-east-2a" = {
       cidr_block        = "10.0.1.0/24"
       availability_zone = "us-east-2a"
+      name = nat
     }
-    "public_subnet_2" = {
+    "us-east-2b" = {
       cidr_block        = "10.0.2.0/24"
       availability_zone = "us-east-2b"
+      name = nat
     }
   }
 }
