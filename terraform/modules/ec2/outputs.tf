62bf55858eefb7f0c16e37f57452a5ea2f2d9a94
= <=>
2025-07-19 16:57:10 +0300

    add private ip output to ec2 module

--- terraform/modules/ec2/outputs.tf
+++ terraform/modules/ec2/outputs.tf
@@ -2,3 +2,8 @@ output "instance_id" {
   description = "The ID of the EC2 instance"
   value       = aws_instance.this.id
 }
+
+output "private_ip" {
+  description = "The private ip of the EC2 instance"
+  value       = aws_instance.this.private_ip
+}
